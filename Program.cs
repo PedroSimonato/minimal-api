fb1d167cffac69d8486fc1257970ea75bc07f1be
Pedro Simonato <pedrohenrryksimonato@gmail.com>
2025-08-12 08:48:57 -0300

    Implementa contexto de banco de dados e entidade Administrador com configuração MySQL

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(optionsAction: options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Olá pessoal");
